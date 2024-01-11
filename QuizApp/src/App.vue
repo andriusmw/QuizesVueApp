a36c97586da30748c73050536166dc63147669d2
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-11 11:55:46 +0100

    building my first vue component

--- QuizApp/src/App.vue
+++ QuizApp/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-  import q from "../src/assets/data/quizes.json"
+  import q from "./data/quizes.json"
   import {ref, watch} from "vue"
+  import Card from "./components/Card.vue"
 
   const quizes = ref(q)
   const search = ref("")
@@ -27,13 +28,14 @@ el valor de search en minuscula; search es ele stado del input/buscador
         <input v-model.trim="search" type="text" placeholder="Search...">
       </header>
       <div class="options-container">
-        <div v-for="quiz in quizes" :key="quiz.id" class="card">
+    <!--    <div v-for="quiz in quizes" :key="quiz.id" class="card"> 
+        
           <img :src="quiz.img" alt="Math-img">
           <div class="card-text">
               <h2>{{quiz.name}}</h2>
               <p> {{quiz.questions.length}} questions </p>
           </div>
-        </div>
+        </div> -->
       </div>
   </div>
 </template>
@@ -71,29 +73,5 @@ el valor de search en minuscula; search es ele stado del input/buscador
     margin-top: 40px;
   }
 
-  /* CARD */
-
-  .card {
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
-    margin-bottom: 35px;
-    margin-right: 20px;
-    cursor: pointer;
-  }
-
-  .card img {
-    width: 100%;
-    height: 190px;
-    margin: 0
-  }
-
-  .card .card-text {
-    padding: 0 5px
-  }
-
-  .card .card-text h2 {
-    font-weight: bold;
-  }
+  
 </style>
