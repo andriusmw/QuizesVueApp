a1decb2f8321f4b461edb62ecc3b0f026029a8f4
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-11 11:24:19 +0100

    rendering cards

--- QuizApp/src/App.vue
+++ QuizApp/src/App.vue
@@ -1,81 +1,86 @@
+<script setup>
+  import q from "../src/assets/data/quizes.json"
+  import {ref} from "vue"
+
+  const quizes = ref(q)
+</script>
+
+
 <template>
   <div class="container">
+   
       <header>
         <h1>Quizes</h1>
         <input type="text" placeholder="Search...">
       </header>
       <div class="options-container">
-        <div class="card">
-          <img src="https://cdn2.vectorstock.com/i/1000x1000/60/81/math-font-with-symbol-and-formula-icon-vector-39626081.jpg" alt="Math-img">
+        <div v-for="quiz in quizes" :key="quiz.id" class="card">
+          <img :src="quiz.img" alt="Math-img">
           <div class="card-text">
-              <h2>Math</h2>
-              <p>15 questions</p>
+              <h2>{{quiz.name}}</h2>
+              <p> {{quiz.questions.length}} questions </p>
           </div>
         </div>
       </div>
   </div>
-</template>z
+</template>
+
+
 
 <style scoped>
-.container {
-  max-width: 100px;
-  margin: 0 auto;
+  .container {
+    max-width: 1000px;
+    margin: 0 auto
+  }
 
-}
+  header {
+    margin-bottom: 10px;
+    margin-top: 30px;
+    display: flex;
+    align-items: center;
+  }
 
-header {
-  margin-bottom: 10px;
-  margin-top: 30px;
-  display: flex;
-  align-items: center;
-}
+  header h1 {
+    font-weight: bold;
+    margin-right: 30px;
+  }
 
-header h1 {
-  font-weight: bold;
-  margin-right: 30px;
+  header input {
+    border: none;
+    background-color: rgba(128,128,128,0.1);
+    padding: 10px;
+    border-radius: 5px;
+  }
 
-}
+  .options-container {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 40px;
+  }
 
-header input {
-  border: none;
-  background-color: rgba(128, 128, 128, 0.1);
-  padding: 10px;
-  border-radius: 5px;
-}
+  /* CARD */
 
-.options-container {
-  display: flex;
-  flex-wrap: wrap;
-  margin-top: 40px;
-}
+  .card {
+    width: 310px;
+    overflow: hidden;
+    border-radius: 2%;
+    box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
+    margin-bottom: 35px;
+    margin-right: 20px;
+    cursor: pointer;
+  }
 
-/* CARD */ 
+  .card img {
+    width: 100%;
+    height: 190px;
+    margin: 0
+  }
 
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-  min-width: max-content;
+  .card .card-text {
+    padding: 0 5px
+  }
 
-}
-
-.card img {
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
-
-.card .card-text {
-  padding: 0 5px;
-
-}
-
-.card .card-text h2 {
-  font-weight: bold;
-}
-
+  .card .card-text h2 {
+    font-weight: bold;
+  }
 </style>
