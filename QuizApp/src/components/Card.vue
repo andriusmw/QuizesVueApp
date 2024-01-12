f071440652c4aa0aab5cc3c28f8341eb3f3893e6
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-12 11:18:32 +0100

    useRouter working

--- QuizApp/src/components/Card.vue
+++ QuizApp/src/components/Card.vue
@@ -1,16 +1,22 @@
 <script setup>
 import {defineProps} from "vue"
+import {useRouter} from "vue-router"
 
-const props = defineProps(['quiz'])
+const router = useRouter()
+const {quiz} = defineProps(['quiz'])
+
+const navigateToQuiz = () => {
+  router.push(`/quiz/${quiz.id}`)
+}
 </script>
 
 
 <template>
-    <div class="card">
+    <div class="card" @click="navigateToQuiz()">
         <img :src="quiz.img" alt="">
         <div class="card-text">
             <h2>{{quiz.name}}</h2>
-            <p>{{quiz.questions.length}} questions</p>
+           <p>{{quiz.questions.length}} questions</p> 
         </div>
     </div>
 </template>
--- QuizApp/src/router/index.js
+++ QuizApp/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import QuizesView from "../views/QuizesView.vue"
+import QuizView from "../views/QuizView.vue"
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,12 @@ const router = createRouter({
             path:"/",
             name: "quizes",
             component: QuizesView
+
+        },
+        {
+            path:"/quiz/:id",
+            name: "quiz",
+            component: QuizView
 
         }
     ]
